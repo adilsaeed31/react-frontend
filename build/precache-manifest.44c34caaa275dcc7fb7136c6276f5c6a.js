a4a16f1d4c880171a56ffd207e9b8dc226d735a7
User1 <user1@User1s-iMac.local>
2019-04-15 13:13:08 +0400

    Resetup

---
+++ build/precache-manifest.44c34caaa275dcc7fb7136c6276f5c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aad9c1fede88318212f3",
+    "url": "/static/js/main.aad9c1fe.chunk.js"
+  },
+  {
+    "revision": "2d99a8ea6b490ed13e96",
+    "url": "/static/js/1.2d99a8ea.chunk.js"
+  },
+  {
+    "revision": "aad9c1fede88318212f3",
+    "url": "/static/css/main.cea0e1bc.chunk.css"
+  },
+  {
+    "revision": "2d99a8ea6b490ed13e96",
+    "url": "/static/css/1.325b6e81.chunk.css"
+  },
+  {
+    "revision": "a4070be8675ff49e48a590668d53c55b",
+    "url": "/index.html"
+  }
+];
